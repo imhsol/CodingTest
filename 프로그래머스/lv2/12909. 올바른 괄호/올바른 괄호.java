96baa89541c8fc4f62983241352b75a5fbeafec8
imhsol <97396131+imhsol@users.noreply.github.com>
2023-02-27 23:37:47 +0900

    [level 2] Title: 올바른 괄호, Time: 20.34 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+	boolean solution(String s) {
+        boolean answer = true;
+        Stack st = new Stack();
+
+        for(int i=0;i<s.length();i++) {
+        	if(s.charAt(i)=='(') {
+        		st.push('(');
+        	} else if(s.charAt(i)==')') {
+        		if(st.empty()) {
+        			return answer = false;
+        		} else {
+        			st.pop();
+        		}
+        	}
+        }
+        return answer = st.empty()? true:false;
+    }
+}
