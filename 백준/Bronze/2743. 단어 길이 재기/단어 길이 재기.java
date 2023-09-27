0c0a579c3d64e8f78d38d4590b5803eb239d83e5
imhsol <97396131+imhsol@users.noreply.github.com>
2023-09-27 23:29:33 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println(s.length());
+	}
+	
+}
