82d69a1eddb837859accd4f9efd64e2719a56c4f
imhsol <97396131+imhsol@users.noreply.github.com>
2023-10-18 20:59:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        
+        ArrayList<String> list1 = new ArrayList<>(Arrays.asList(cards1));
+		ArrayList<String> list2 = new ArrayList<>(Arrays.asList(cards2));
+		String answer = "Yes";
+		
+		for(int i=0; i<goal.length; i++){
+			if(list1.size()>0 && goal[i].equals(list1.get(0))){
+				list1.remove(0);
+			} else if(list2.size()>0 && goal[i].equals(list2.get(0))){
+				list2.remove(0);
+			} else{
+				answer = "No";
+			}
+		}
+		
+		return answer; 
+    }
+}
